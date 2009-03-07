63c768c3e5802758466b66bd3be2f8896382a455
Daniel Higginbotham <daniel@daniel-higginbothams-macbook.local>
2009-03-06 20:36:29 -0500

    before trying to add logs

---
+++ Classes/MedicationLog.h
@@ -0,0 +1,16 @@
+//
+//  MedicationLog.h
+//  RegularReminder
+//
+//  Created by Daniel Higginbotham on 3/6/09.
+//  Copyright 2009 flyingmachinestudios.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MedicationLog : NSObject {
+
+}
+
+@end
---
+++ Classes/MedicationLog.m
@@ -0,0 +1,14 @@
+//
+//  MedicationLog.m
+//  RegularReminder
+//
+//  Created by Daniel Higginbotham on 3/6/09.
+//  Copyright 2009 flyingmachinestudios.com. All rights reserved.
+//
+
+#import "MedicationLog.h"
+
+
+@implementation MedicationLog
+
+@end
