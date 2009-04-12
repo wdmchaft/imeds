a946e7ce94626f9c9d709e4e3b54d0d74b62f492
Daniel Higginbotham <daniel@daniel-higginbothams-macbook.local>
2009-04-11 23:54:31 -0400

    edit a medication

--- Classes/MedicationDetailViewController.m
+++ Classes/MedicationDetailViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "MedicationDetailViewController.h"
+#import "MedicationEditController.h"
 #import "Medication.h"
+#import "RegularReminderAppDelegate.h"
 
 @implementation MedicationDetailViewController
 
@@ -16,6 +18,16 @@
 #pragma mark Standard
 -(void)viewDidLoad {
 	nameLabel.font = [UIFont systemFontOfSize:30];
+  
+  UIBarButtonItem *editButton = [[UIBarButtonItem alloc] 
+																 initWithTitle:@"Edit"
+																 style:UIBarButtonItemStyleBordered
+																 target:self 
+																 action:@selector(edit)]; 
+	self.navigationItem.rightBarButtonItem = editButton; 
+  [editButton release];
+  
+  [super viewDidLoad];
 }
 
 -(void)setupLabels
@@ -38,6 +50,15 @@
 	[takeAgainExactLabel release];
 	[takeAgainRelativeLabel release];
   [super dealloc];
+}
+
+#pragma mark -
+- (void) edit {
+  MedicationEditController * controller = [[MedicationEditController alloc] initWithNibName:@"MedicationEdit" bundle:nil];
+	controller.medication = medication;
+	RegularReminderAppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+	[delegate.navigationController pushViewController:controller animated:YES];
+	[controller release];
 }
 
 
